9ab33e6d1e6003ab6b5b843d8ef24ccbc3664288
Ibrahim Noor Shafin <ibrahimnoorsf@gmail.com>
2021-11-13 20:34:33 +0600

    bonus section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../Shared/Footer/Footer';
 import NavBar from '../Shared/NavBar/NavBar';
 import Header from './Header/Header';
+import Offers from './Offers/Offers';
 import Products from './Products/Products';
 import Reviews from './Reviews/Reviews';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <NavBar></NavBar>
             <Header></Header>
             <Products></Products>
+            <Offers></Offers>
             <Reviews></Reviews>
             <Footer></Footer>
         </div>
---
+++ src/pages/Home/Offers/Offers.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Offers = () => {
+    return (
+        <div className="container mt-5">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card">
+                        <img src="https://template.hasthemes.com/garcia/garcia/assets/images/banner/banner-6.jpg" class="card-img-top" alt="..." />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://template.hasthemes.com/garcia/garcia/assets/images/banner/banner-7.jpg" class="card-img-top" alt="..." />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Offers;
