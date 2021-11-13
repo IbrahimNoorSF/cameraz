eba928380e28894d6c7a225e73996a3c3bdb787d
Ibrahim Noor Shafin <ibrahimnoorsf@gmail.com>
2021-11-13 23:36:31 +0600

    maked site more matured

--- src/pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/pages/Dashboard/DashboardHome/DashboardHome.js
@@ -3,8 +3,8 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div className="text-center">
+            <h1 className="mt-5">Welcome!</h1>
             <h1 className="mt-3 p-3">On The Left Side You Can Find Options That Can Be Used On Many Things</h1>
-            <img className="mt-5 rounded img-fluid" src="https://www.thegoandroid.com/wp-content/uploads/2018/03/tenor_1.gif" alt="" />
         </div>
     );
 };
